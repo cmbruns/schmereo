f29e37d496190d50ef3d28558a17b0673f5456b2
Christopher Bruns <cmbruns@rotatingpenguin.com>
2019-10-12 09:05:49 -0700

    populate setup.py save example image

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+# Load module version from ovr/version.py
+__version__ = '0.0.0'  # value will be replaced on the next line
+exec(open('schmereo/version.py').read())
+
+setup(
+    name="schmereo",
+    version=__version__,
+    author="Christopher M. Bruns",
+    author_email="cmbruns@rotatingpenguin.com",
+    description="stereograph restoration application",
+    url="https://github.com/cmbruns/schmereo",
+    download_url='https://github.com/cmbruns/schmereo/tarball/' + __version__,
+    packages=["schmereo"],
+    keywords="stereograph stereoscopic 3D restoration",
+    license="GPL",
+    install_requires=["numpy", "pillow", "PyOpenGL", "PyQt5"],
+    extras_require={}
+)
